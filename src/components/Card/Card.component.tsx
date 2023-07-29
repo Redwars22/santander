632f3e7ed16882102c25ac5e0381bbe6e753bf7a
AndrewNation <andrewpereira.megabyte@gmail.com>
2023-07-29 00:49:34 -0300

    adicionando array de dep. vazio no useEffect

--- src/components/Card/Card.component.tsx
+++ src/components/Card/Card.component.tsx
@@ -15,7 +15,7 @@ export default function CardComponent() {
             limit: 0,
             number: "0000"
         }
-    });
+    }, []);
 
     useEffect(()=>{
         async function getData(){
@@ -37,7 +37,7 @@ export default function CardComponent() {
         }
 
         getData();
-    })
+    }, [])
 
     return (
         <div className={`${style.cardContainer} ${style.shadowBox}`}>

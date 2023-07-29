94921b15667c79410d55d6761f21210579f578b0
AndrewNation <andrewpereira.megabyte@gmail.com>
2023-07-29 13:18:51 -0300

    badge andrewnation

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -20,7 +20,7 @@ export default function Home(){
                 </div>
                 <CarouselComponent/>
             </div>
-            <AndrewNationBadgeComponent mode={"dark"}/>
+            <AndrewNationBadgeComponent mode={"light"}/>
         </>
     )
 }
